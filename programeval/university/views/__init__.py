c9e4844a22d278b9ac6d2511c582e751c1f3e97c
wasaycmu <144651557+wasaycmu@users.noreply.github.com>
2023-12-05 00:51:13 -0600

    objectives implemented

--- programeval/university/views/__init__.py
+++ programeval/university/views/__init__.py
@@ -3,3 +3,4 @@ from university.views.department_views import *
 from university.views.faculty_views import *
 from university.views.program_views import *
 from university.views.course_views import *
+from university.views.objective_views import *
